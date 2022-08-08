f5cd739b6da98b4b30bc2858cd9facabb74e755c
AntonJurkovskiy <azimut4946777@gmail.com>
2022-08-08 07:43:17 +0300

    add 1 Fragment and Nav Graf

--- app/src/main/java/com/azimut4946777/secretmessage/EncryptFragment.kt
+++ app/src/main/java/com/azimut4946777/secretmessage/EncryptFragment.kt
@@ -6,19 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.findNavController
 
-class WelcomeFragment : Fragment() {
+
+class EncryptFragment : Fragment() {
+
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
-        val startButton = view.findViewById<Button>(R.id.b_start)
-        startButton.setOnClickListener {
-                   view.findNavController()
-                       .navigate(R.id.action_welcomeFragment_to_messageFragment)
-        }
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_encrypt, container, false)
+        val message  =EncryptFragmentArgs.fromBundle(requireArguments()).message
+        val encryptedView = view.findViewById<TextView>(R.id.encrypted_message)
+        encryptedView.text = message.reversed()
+
+
 
 
         return view
